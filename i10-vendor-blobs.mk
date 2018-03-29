904719c53fb743e07d3eb1c440a495b6f0a77fb4
Ahad Kabir Siddique <ahad.blogger24@gmail.com>
2018-03-29 16:33:58 +0600

    init

---
+++ i10-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/i10/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/i10/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/i10/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/i10/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/i10/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/i10/vendor,system/vendor)
